2c4fe76372c0466cbbd93aab2f6a8d4b777769dd
Stan Choi <sjc591@nyu.edu>
2025-06-03 14:22:29 -0400

    init

---
+++ src/test/java/com/jiggycode/LyricsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiggycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
