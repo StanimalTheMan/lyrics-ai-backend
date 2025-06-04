6a0e9446356a30ab7b6ca4eafdee8a6d147c5357
Stan Choi <sjc591@nyu.edu>
2025-06-04 11:43:59 -0400

    Try to fetch song from Spotify

--- src/main/java/com/jiggycode/SongController.java
+++ src/main/java/com/jiggycode/SongController.java
@@ -1,4 +1,27 @@
 package com.jiggycode;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/songs")
 public class SongController {
+
+    private final SpotifyApiService spotifyApiService;
+
+    public SongController(SpotifyApiService spotifyApiService) {
+        this.spotifyApiService = spotifyApiService;
+    }
+
+    @GetMapping("/search")
+    public String searchSongs(
+            @RequestParam String track,
+            @RequestParam String artist
+    ) throws Exception {
+        return spotifyApiService.searchExactMatchTrack(track, artist);
+    }
+
+
 }
